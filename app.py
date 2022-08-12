6a0c91de202f8f6c51285fbdd7f3d6605ceb40b2
bzakarya <z.bengati@gmail.com>
2022-08-13 00:26:24 +0200

    test 6

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 
 # rest of connection code using the connection string `uri`
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri, 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(uri)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
